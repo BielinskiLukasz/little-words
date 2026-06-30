6bb36ea97d1dcc3d6d7fe6bf18ca0bad12caa7b7
Lukasz Bielinski <lukasz.pawel.bielinski@gmail.com>
2026-06-30 13:47:10 +0200

    feat(01-03): Implement react-i18next with Polish default and locale files

---
+++ src/i18n/i18n.d.ts
@@ -0,0 +1,14 @@
+import 'i18next'
+
+import type enCommon from './locales/en/common.json'
+import type enOnboarding from './locales/en/onboarding.json'
+
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    defaultNS: 'common'
+    resources: {
+      common: typeof enCommon
+      onboarding: typeof enOnboarding
+    }
+  }
+}
